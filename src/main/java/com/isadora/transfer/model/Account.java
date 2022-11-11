c371885de9a09c174ec4ded156f86f36ce64f9f2
Isadora Palhoti <isadorapalhoti@gmail.com>
2022-11-11 13:09:11 -0300

    Criacao da classe DTO, add as dependências do banco de dados e setando as propriedades do banco

--- src/main/java/com/isadora/transfer/model/Account.java
+++ src/main/java/com/isadora/transfer/model/Account.java
@@ -2,17 +2,24 @@ package com.isadora.transfer.model;
 
 import java.time.LocalDateTime;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-@Setter
-@Getter
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Account {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer id;
 	String accountOring;
 	String accountDestin;
 	double velue;
--- src/main/java/com/isadora/transfer/model/dto/AccountDto.java
+++ src/main/java/com/isadora/transfer/model/dto/AccountDto.java
@@ -1,5 +1,22 @@
 package com.isadora.transfer.model.dto;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountDto {
+	
+	private String accountOring;
+	private String accountDestin;
+	private double velue;
+	private LocalDateTime dateScheduled;
+	private LocalDateTime dateTransfer;
 
 }
