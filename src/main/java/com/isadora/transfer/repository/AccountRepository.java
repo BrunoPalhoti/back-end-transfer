4e81e5bde0c7f51dcf65cf9aea82236e997d9398
Isadora Palhoti <isadorapalhoti@gmail.com>
2022-11-11 15:21:09 -0300

    update na camada de repository

--- src/main/java/com/isadora/transfer/repository/AccountRepository.java
+++ src/main/java/com/isadora/transfer/repository/AccountRepository.java
@@ -1,5 +1,9 @@
 package com.isadora.transfer.repository;
 
-public interface AccountRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.isadora.transfer.model.Account;
+
+public interface AccountRepository extends JpaRepository<Account, Integer> {
 
 }
--- src/main/java/com/isadora/transfer/repository/RateRepository.java
+++ src/main/java/com/isadora/transfer/repository/RateRepository.java
@@ -1,5 +1,9 @@
 package com.isadora.transfer.repository;
 
-public interface RateRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.isadora.transfer.model.Rate;
+
+public interface RateRepository extends JpaRepository<Rate, Integer> {
 
 }
